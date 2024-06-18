91508f9693d83eea15a1cad4a1b39e2ed6f672d8
Guilherme Simões <guilhermessimoescontato@gmail.com>
2024-06-18 17:23:06 -0300

    vindo null

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,8 +20,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 		System.out.println(json);
 
-		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-
+		ConverteDados converteDados = new ConverteDados();
+		DadosSerie dados = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
---
+++ src/main/java/br/com/alura/screenmatch/service/IConverteDados.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatch.service;
+public interface IConverteDados {
+    <T> T obterDados (String json, Class<T> classe);
+}
