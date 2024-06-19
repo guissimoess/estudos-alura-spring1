c0f5a46c407c591d020d2558e7d08af9644868ff
Guilherme Simões <guilhermessimoescontato@gmail.com>
2024-06-19 02:32:20 -0300

    utilizando funcoes lambda

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,44 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
+    public void exibeMenu () {
+        System.out.print("Digite o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+		for (int i=1; i<= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			DadosTemporada temporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(temporada);
+		}
+		temporadas.forEach(System.out::println);
+
+//        for (int i=0; i<dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodios = temporadas.get(i).episodios();
+//            for (int j=0; j<episodios.size(); j++) {
+//                System.out.println(episodios.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+    }
+}
